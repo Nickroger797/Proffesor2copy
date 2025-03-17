7f3f0d9fa3b502fe7ecbf8894a2f03285f1fcdbf
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-17 11:49:43 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -66,6 +66,7 @@ class Database:
         self.grp = self.db.groups
         self.users = self.db.uersz
         self.bot = self.db.clone_bots
+        self.botcol = self.db["deendayal"]
 
 
     def new_user(self, id, name):
