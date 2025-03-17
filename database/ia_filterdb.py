15fe4d7f9da794a868163481ea75301aaa1fc014
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-18 01:24:17 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -8,6 +8,7 @@ from pyrogram.file_id import FileId
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
 from .Imdbposter import get_movie_details, fetch_image
+from utils import get_settings, save_group_settings, temp, get_status
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import FILE_DB_URI, SEC_FILE_DB_URI, DATABASE_NAME, COLLECTION_NAME, MULTIPLE_DATABASE, USE_CAPTION_FILTER, MAX_B_TN, MOVIE_UPDATE_CHANNEL, OWNERID
 
