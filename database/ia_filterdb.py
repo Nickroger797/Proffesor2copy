b4c7e86278cb45bac1e7a427d6581ebda4ddf4dd
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-17 12:53:14 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -8,7 +8,7 @@ from pyrogram.file_id import FileId
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
 from .Imdbposter import get_movie_details, fetch_image
-from info import FILE_DB_URI, SEC_FILE_DB_URI, DATABASE_NAME, COLLECTION_NAME, MULTIPLE_DATABASE, USE_CAPTION_FILTER, MAX_B_TN, MOVIE_UPDATE_CHANNEL
+from info import FILE_DB_URI, SEC_FILE_DB_URI, DATABASE_NAME, COLLECTION_NAME, MULTIPLE_DATABASE, USE_CAPTION_FILTER, MAX_B_TN, MOVIE_UPDATE_CHANNEL, OWNERID
 
 # First Database For File Saving 
 client = MongoClient(FILE_DB_URI)
