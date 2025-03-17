8bfb508af998ab93269052542b5e40f08a1e11b0
Aditya Prakash <115403112+Nickroger797@users.noreply.github.com>
2025-03-17 10:33:46 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -173,4 +173,64 @@ def unpack_new_file_id(new_file_id):
         )
     )
     return file_id
-    
+
+async def send_msg(bot, filename, caption): 
+    try:
+        filename = re.sub(r'\(\@\S+\)|\[\@\S+\]|\b@\S+|\bwww\.\S+', '', filename).strip()
+        caption = re.sub(r'\(\@\S+\)|\[\@\S+\]|\b@\S+|\bwww\.\S+', '', caption).strip()
+        
+        year_match = re.search(r"\b(19|20)\d{2}\b", caption)
+        year = year_match.group(0) if year_match else None
+
+        pattern = r"(?i)(?:s|season)0*(\d{1,2})"
+        season = re.search(pattern, caption) or re.search(pattern, filename)
+        season = season.group(1) if season else None 
+
+        if year:
+            filename = filename[: filename.find(year) + 4]  
+        elif season and season in filename:
+            filename = filename[: filename.find(season) + 1]
+
+        qualities = ["ORG", "org", "hdcam", "HDCAM", "HQ", "hq", "HDRip", "hdrip", "camrip", "CAMRip", "hdtc", "predvd", "DVDscr", "dvdscr", "dvdrip", "dvdscr", "HDTC", "dvdscreen", "HDTS", "hdts"]
+        quality = await get_qualities(caption.lower(), qualities) or "HDRip"
+
+        language = ""
+        possible_languages = CAPTION_LANGUAGES
+        for lang in possible_languages:
+            if lang.lower() in caption.lower():
+                language += f"{lang}, "
+        language = language[:-2] if language else "Not idea 😄"
+
+        filename = re.sub(r"[\(\)\[\]\{\}:;'\-!]", "", filename)
+
+        text = "#𝑵𝒆𝒘_𝑭𝒊𝒍𝒆_𝑨𝒅𝒅𝒆𝒅 ✅\n\n👷𝑵𝒂𝒎𝒆: `{}`\n\n🌳𝑸𝒖𝒂𝒍𝒊𝒕𝒚: {}\n\n🍁𝑨𝒖𝒅𝒊𝒐: {}"
+        text = text.format(filename, quality, language)
+
+        if await add_name(OWNERID, filename):
+            imdb = await get_movie_details(filename)  
+            resized_poster = None
+
+            if imdb:
+                poster_url = imdb.get('poster_url')
+                if poster_url:
+                    resized_poster = await fetch_image(poster_url)  
+
+            filenames = filename.replace(" ", '-')
+            btn = [[InlineKeyboardButton('🌲 Get Files 🌲', url=f"https://telegram.me/{temp.U_NAME}?start=getfile-{filenames}")]]
+            
+            if resized_poster:
+                await bot.send_photo(chat_id=MOVIE_UPDATE_CHANNEL, photo=resized_poster, caption=text, reply_markup=InlineKeyboardMarkup(btn))
+            else:              
+                await bot.send_message(chat_id=MOVIE_UPDATE_CHANNEL, text=text, reply_markup=InlineKeyboardMarkup(btn))
+
+    except:
+        pass
+
+async def get_qualities(text, qualities: list):
+    """Get all Quality from text"""
+    quality = []
+    for q in qualities:
+        if q in text:
+            quality.append(q)
+    quality = ", ".join(quality)
+    return quality[:-2] if quality.endswith(", ") else quality
